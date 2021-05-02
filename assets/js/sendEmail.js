b1b6fb5732c44a3e3d682d2a89c34a88329fa4c8
Justin-Sawyer <justin.sawyer@icloud.com>
2021-05-02 09:59:32 +0000

    formatting of previous-role pages, emailjs working and recd / error messages appear

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -7,9 +7,13 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            $("#successOrFailure-en").text("Your mail has been sent.")
+            $("#successOrFailure-fr").text("Votre mail a été reçu.")
         },
         function(error) {
             console.log("FAILED", error);
+            $("#successOrFailure-en").html("<p>An error occured. Please try again.</p><a href='mailto: justin.sawyer.dev@gmail.com'>Or Click Here</a>")
+            $("#successOrFailure-fr").html("<p>Une erreur s'est produite. Veuillez réessayer.</p><a href='mailto: justin.sawyer.dev@gmail.com'>Ou cliquez ici</a>")
         }
     );
     return false;  // To block from loading a new page
