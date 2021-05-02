c653b47010e9aeefa9585698434d77466d501e45
Justin-Sawyer <justin.sawyer@icloud.com>
2021-05-02 11:35:24 +0000

    interests: map works and text translated into fr

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,16 +1,16 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 8,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 43.7102,
+            lng: 7.2620
         }
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
-        { lat: 40.785091, lng: -73.968285 },
+        { lat: 43.7102, lng: 7.2620 },
         { lat: 41.084045, lng: -73.874245 },
         { lat: 40.754932, lng: -73.984016 }
     ];
