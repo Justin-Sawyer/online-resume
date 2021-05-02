32c3c968c26b1e1e6e32ffde52a749790bae0c9f
Justin-Sawyer <justin.sawyer@icloud.com>
2021-05-02 09:31:52 +0000

    formatting of previous-role pages, emailjs working

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "rosie", {
+    emailjs.send("service_u36nneq","template_tackgkn", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
